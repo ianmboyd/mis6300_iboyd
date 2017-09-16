25359d2bc37eedea5a4602cf5fa4adfefba6d257
ianmboyd <iboyd@babson.edu>
2017-09-16 11:02:30 -0400

    SQL Exercise 1
    Submissoin for SQL excercise 1

---
+++ SQL_Homework/SQL_Excercise_1.sql
@@ -0,0 +1,10 @@
+select TOP(1000) 
+s.SalesOrderID, 
+s.OrderQty, 
+s.ProductID, 
+p.Name,
+s.UnitPrice, 
+s.LineTotal
+ from [AdventureWorks2012].[Sales].[SalesOrderDetail] as s, 
+ [AdventureWorks2012].[Production].[Product] as p
+where s.ProductID = p.ProductID
